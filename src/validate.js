f428edb02fdba0661b9a2fb70403387401238230
macdonst <simon.macdonald@gmail.com>
2022-08-17 00:51:36 -0400

    Only convert when not JSON
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- src/validate.js
+++ src/validate.js
@@ -5,7 +5,7 @@ import convertToSchema from './convert-to-schema.js'
 
 export default function validator (request, schema) {
   let data = request.body
-  if (isJSON(request)) {
+  if (!isJSON(request)) {
     let obj = convertToSchema(data)
     data = formEncodingToSchema(obj, schema)
   }
